a9589a3cc390fe7bcba2ecd6bca4489648d3047b
Conor Sheppard <conorsheppard@Conors-MacBook-Pro.local>
2021-12-12 23:13:20 +0000

    add two numbers leetcode problem

---
+++ internal/add-two-numbers/add-two-numbers-first-solution.go
@@ -0,0 +1,85 @@
+package add_two_numbers
+
+import (
+"fmt"
+"strconv"
+)
+
+// AddTwoNumbers problem: https://leetcode.com/problems/add-two-numbers/
+func AddTwoNumbersFirstSolution(l1, l2 *ListNode) *ListNode {
+	list1Total := extractNumber(l1)
+	list2Total := extractNumber(l2)
+
+	combinedNums := list1Total + list2Total
+
+	result := addNextNodes(strconv.Itoa(combinedNums))
+	return result
+}
+
+func extractNumber(list *ListNode) int {
+	list1NumStr := ""
+	for {
+		if list.Next != nil {
+			list1NumStr = strconv.Itoa(list.Val) + list1NumStr
+		} else {
+			list1NumStr = strconv.Itoa(list.Val) + list1NumStr
+			break
+		}
+		list = list.Next
+	}
+
+	list1NumInt, err := strconv.Atoi(list1NumStr)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return list1NumInt
+}
+
+func addNextNodes(combinedNums string) *ListNode {
+	valAsStr := string(combinedNums[len(combinedNums)-1])
+	val, _ := strconv.Atoi(valAsStr)
+
+	if len(combinedNums)-1 == 0 {
+		return &ListNode{
+			Val: val,
+			Next: nil,
+		}
+	}
+
+	combinedNums = combinedNums[:len(combinedNums)-1]
+	return &ListNode{
+		Val:  val,
+		Next: addNextNodes(combinedNums),
+	}
+}
+
+func acceptedSolution(l1, l2 *ListNode) *ListNode {
+	cur := new(ListNode)
+	ret := cur
+	sum := 0
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			sum = sum + l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum = sum + l2.Val
+			l2 = l2.Next
+		}
+
+		cur.Next = &ListNode{
+			sum%10,
+			nil,
+		}
+		cur = cur.Next
+		sum = sum/10
+	}
+
+	if sum > 0 {
+		cur.Next = &ListNode{sum,nil}
+	}
+
+	return ret.Next
+}
---
+++ internal/add-two-numbers/add-two-numbers.go
@@ -0,0 +1,52 @@
+package add_two_numbers
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+// AddTwoNumbers problem: https://leetcode.com/problems/add-two-numbers/
+func AddTwoNumbers(l1, l2 *ListNode) *ListNode {
+	list1 := l1
+	list2 := l2
+	var carryOver int
+	currentNode := &ListNode{Val:  0, Next: nil}
+	result := currentNode
+	for list1 != nil || list2 != nil {
+		var list1Val int
+		var list2Val int
+		if list1 == nil {
+			list1Val = 0
+		} else {
+			list1Val = list1.Val
+		}
+		if list2 == nil {
+			list2Val = 0
+		} else {
+			list2Val = list2.Val
+		}
+		// sum both nodes
+		sum := list1Val + list2Val + carryOver
+		currentNode.Val = sum % 10
+		carryOver = sum / 10
+		if list1 != nil {
+			list1 = list1.Next
+		}
+		if list2 != nil {
+			list2 = list2.Next
+		}
+		// adding if so that a new currentNode with a Val of 0 is not created at the end and doesn't add an extra 0
+		// to the result ListNode (e.g. 7080)
+		if list1 != nil || list2 != nil || carryOver != 0 {
+			currentNode.Next = &ListNode{Val:  0, Next: nil}
+			// two lines need to be treated as one transaction, otherwise nil pointer will occur as currentNode.Next
+			// will be nil
+			currentNode = currentNode.Next
+		}
+		// this is needed for when the loop breaks and the final carryover becomes the most significant digit
+		if carryOver != 0 {
+			currentNode.Val = carryOver
+		}
+	}
+	return result
+}
